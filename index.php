bf9e2a02df39dd5d1a465199f2271c7737c91714
Anakareli Gonzalez <anakgonzalez@csumb.edu>
2018-02-05 07:40:55 +0000

    Last update

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        
+        <meta charset = "utf-8"/>
+        <title>Anakareli Gonzalez: Personal Website</title>
+        
+        <link href = "css/styles.css" rel = "stylesheet" type = "text/css" />
+        <link href="<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">"
+        
+    </head>
+<!-- closing head -->
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1>Anakareli Gonzalez</h1>
+        </header>
+        <nav>
+            <hr width = "50%" />
+            <a href="index.html" style="color:#098dfb">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <br /><br />
+        
+        <main>
+            
+            <figure>
+                <img src="/cst-336/portfolio/cst336_lab1/img/ag.png" alt="Picture of Anakareli Gonzalez"></img>
+            </figure>
+            
+            <div>
+                Hello! <br />
+                <p>Thank you for visiting my proffessional portfolio website!</p>
+                
+                <p>I am an aspiring Software Engineer.</p>
+                
+                <p>Feel free to contact me.</p>
+                
+                <br /><br />
+                
+                <em>With ordinary talent and extraordinary<strong> perseverance</strong>, all things are attainable</em><br />
+                -Thomas F. Buxton
+                
+            </div>
+            
+        </main>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            
+            
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
